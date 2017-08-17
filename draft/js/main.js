3526c0b0eecacec7fbbcb82af9f7f93b97bad4a8
Cham K <chamkank@users.noreply.github.com>
2017-08-17 15:37:23 -0700

    Put JS into separate files

---
+++ draft/js/main.js
@@ -0,0 +1,17 @@
+AOS.init({once: true}); // for scroll animations
+
+function expandMenu() {  // For when hamburger menu is clicked
+  var nav = document.getElementById("main-navbar");
+  var links = document.getElementById("link-container");
+  if (nav.className === "navbar") {
+    nav.className += " responsive";
+    nav.className
+  } else {
+    nav.className = "navbar";
+  }
+  if (links.className.includes("align-center")){
+    links.className = "align-items-right"
+  } else {
+    links.className += " align-center";
+  }
+}
